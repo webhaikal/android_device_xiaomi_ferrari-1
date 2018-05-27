b91d8581013107ff77b420b1d04c6334f34ab37f
Haikal Izzuddin <haikalizz05@gmail.com>
2018-05-27 13:42:52 +0800

    Remove libshim_get_process_name

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -25,14 +25,6 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := get_process_name/get_process_name.c
-LOCAL_MODULE := libshims_get_process_name
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := atomic.cpp
 LOCAL_MODULE := libshim_atomic
 LOCAL_MODULE_TAGS := optional

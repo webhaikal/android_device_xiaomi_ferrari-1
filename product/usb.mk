1e49f4bbba19a8638d7f406353bbceb44a0a46b0
Haikal Izzuddin <haikalizz05@gmail.com>
2018-05-27 20:23:40 +0800

    Change additional property to default property override

--- product/usb.mk
+++ product/usb.mk
@@ -3,6 +3,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
+# force enable ADB
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
+    persist.sys.usb.config=mtp,adb \
+    persist.sys.root_access=3
+	
 # USB ID
 #ADDITIONAL_DEFAULT_PROPERTIES += \
 #    ro.usb.id.midi=90BA \

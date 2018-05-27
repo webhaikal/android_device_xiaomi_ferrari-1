c5f9fe91f249ae00cbd8bd3777bcece7c6dab7d2
Haikal Izzuddin <haikalizz05@gmail.com>
2018-05-27 12:58:38 +0800

    QCamera2: Fixed compilation error for AOSP upgrade

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -2,3 +2,5 @@ LOCAL_32_BIT_ONLY := true
 ifeq ($(TARGET_ARCH),$(filter $(TARGET_ARCH),arm arm64))
   include $(call all-subdir-makefiles)
 endif
+
+LOCAL_CFLAGS := -Wall -Wextra -Werror -Wno-unused-parameter -Wno-unused-variable

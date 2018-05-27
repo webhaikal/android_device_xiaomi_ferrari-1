6045b6a939c94f54cbbda275fba3e6a0a8f60949
Haikal Izzuddin <haikalizz05@gmail.com>
2018-05-27 13:53:32 +0800

    Fix kernel compile

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,4 +1,5 @@
 # Kernel
 TARGET_KERNEL_CONFIG := ferrari_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ferrari
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
